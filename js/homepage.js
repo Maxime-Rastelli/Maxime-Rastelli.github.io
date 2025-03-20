56d5e15d435f0da84eaa7a44a484d7e84bbc87fe
Sogata <maximer819@gmail.com>
2025-03-20 21:51:33 +0100

    Ajout d'affichage d'erreur

--- js/homepage.js
+++ js/homepage.js
@@ -1,12 +1,27 @@
 import { view } from "./view/viewHomepage.js";
 
+let search = function(){
+    view.invalidText.style.visibility = "hidden";
+    view.inputSearch.style.border = "none";
+    if(view.inputSearch.value == ""){
+        view.invalidText.style.visibility = "visible";
+        view.inputSearch.style.border = "red solid 2px";
+    }else{
+        let recherche = "main.html?search=" + view.inputSearch.value;
+        window.location.href =  recherche;
+    }
+}
+
 view.btnSearch.addEventListener("click", function () {
-    let recherche = "main.html?search=" + view.inputSearch.value;
-    window.location.href =  recherche;
+    search();
 });
 
 view.btnGetStarted.addEventListener("click", function () {
-    let recherche = "main.html?search=" + view.inputSearch.value;
-    window.location.href =  recherche;
+    search();
 })
 
+view.inputSearch.addEventListener("keydown", async function(event){
+    if(event.key == "Enter"){
+        search();
+    }
+});
--- js/view/viewHomepage.js
+++ js/view/viewHomepage.js
@@ -2,8 +2,10 @@ export const view = {
     
     btnSearch: document.querySelector(".search"),
 
-    btnGetStarted: document.querySelector(".getStarted"),
+    btnGetStarted: document.querySelector(".button_get_started"),
 
     inputSearch: document.querySelector(".search_bar"),
+
+    invalidText: document.querySelector(".invalid_input"),
     
 };
